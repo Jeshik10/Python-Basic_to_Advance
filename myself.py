d8a822732db0661d27c634f1d30be02fbb6b2431
Jeshik10 <phuyaljeshik10@gmail.com>
2024-06-17 20:09:07 +0545

    first commit

---
+++ myself.py
@@ -0,0 +1,11 @@
+# ALL INFO
+
+print("Name: JESHIK PHUYAL")
+print("Location: KTM")
+print("Position: student")
+print("Gmail: phuyaljeshik10@gmail.com")
+print("college: Nepal Engineering College")
+print("Hobby: Book reading")
+print("fav color: Black")
+print("fav movie: fight club")
+print("fav animal: Panda")
